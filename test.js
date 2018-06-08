3ca4170f7ac90708b0ae23207a01314e5b58ca4a
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-06-08 19:42:45 +0300

    Happy test case.

--- test.js
+++ test.js
@@ -1,14 +1,35 @@
 import test from 'ava';
 import m from '.';
 
-test('TODO', /*async*/ t => {
-  m('TODO');
-  t.true(true);
-  /*const token = '523ef691191741c99d5afbcfe58079bfa0038771';
-  const repos1 = await m('octocat', {token});
-  const repos2 = await m('octocat', {token});
+test('fetches commits and PRs', async t => {
+  /* AurelienLourot had the following activity:
+     * 2017-08-26: nothing
+     * 2017-08-27: 14 commits in AurelienLourot/mybeir.ut
+     * 2017-08-28: 1 PR in tt-gf/ant-ivy */
 
-  t.truthy(repos1.length);
-  t.truthy(repos2.length);
-  t.deepEqual(repos1, repos2);*/
+  const ora = string => {
+    if (string) {
+      console.log(string);
+    }
+    return {
+      start(string) {
+        if (string) {
+          console.log(string);
+        }
+        return this;
+      },
+      stop() {},
+      succeed(string) {
+        if (string) {
+          console.log(string);
+        }
+      },
+      warn() {},
+    };
+  };
+
+  const result = await m('AurelienLourot', '2017-08-26', '2017-08-28', ora, console);
+  t.is(result.size, 2);
+  t.true(result.has('AurelienLourot/mybeir.ut'));
+  t.true(result.has('tt-gf/ant-ivy'));
 });
